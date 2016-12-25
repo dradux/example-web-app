370585923d3fa509ba6907c405327c3bd84864c7
David Rader <sa@adercon.com>
2016-12-25 15:39:25 -0500

    reset VERSION_STR back to 0.0.1

--- routes.py
+++ routes.py
@@ -1,11 +1,11 @@
 from app import app
 import os
 
-version = os.getenv('VERSION_STR', '0.5.6')
+version = os.getenv('VERSION_STR', '0.0.1')
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World! " + version
-	# @TODO see the other exampe-* apps for additional examples.
-	# NOTICE: please fork this to your own repo before committing.
+    # @TODO see the other exampe-* apps for additional examples.
+    # NOTICE: please fork this to your own repo before committing.
